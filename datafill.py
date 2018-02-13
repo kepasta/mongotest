62c08665c564d2adfe17bc635bf6e4d3afd2e1a2
kepasta <36231596+kepasta@users.noreply.github.com>
2018-02-13 13:47:19 +0300

    Add files via upload

---
+++ datafill.py
@@ -0,0 +1,22 @@
+import time
+import pymongo
+import dtppl
+
+m = pymongo.MongoClient()
+
+
+i = 0
+
+while (i < 200):
+
+    doc = dtppl.pack(dtppl.name, dtppl.country, dtppl.number, dtppl.digits)
+
+    start = time.time()
+    m.tests.insertTest.insert(doc, manipulate=False, w=1)
+    end = time.time()
+
+    executionTime = (end - start) * 1000 # Convert to ms
+
+    print(executionTime)
+
+    i = i + 1
---
+++ dtppl.py
@@ -0,0 +1,24 @@
+import random
+
+def pack(name, country, number, digits):
+    rname = name[random.randrange(len(name))]
+    randcn = random.randrange(len(country))
+    rcountry = country[randcn]
+    n = 0
+    rnumber = number[randcn]
+    if randcn == 1 or randcn == 2 or randcn == 4:
+        while n < 9:
+            rnumber += digits[random.randrange(10)]
+            n += 1
+    else:
+        while n < 10:
+            rnumber += digits[random.randrange(10)]
+            n += 1
+    return {'name' : rname, 'country' : rcountry, 'number' : rnumber}
+
+name = ['John', 'James', 'Hugh', 'Sam', 'Michael', 'Quentin', 'Harry', 'Gary', 'Ryan', 'Patrick', 'Daniel', 'Rick',
+        'Aaron', 'Robert', 'Richard',
+        'Tobey', 'Tom', 'Carl', 'Peter', 'Tyron', 'Chris', 'Benjamin', 'Bryan', 'Barry', 'Ivan', 'George']
+country = ['USA', 'Great Britain', 'France', 'Russian Federation', 'China', 'Canada']
+number = ['+1', '+44', '+33', '+7', '+86', '+1']
+digits = '1234567890'
