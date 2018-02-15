0f0e839bf3af9c9c2f6c6af9d78170fc8ea1073f
kepasta <36231596+kepasta@users.noreply.github.com>
2018-02-15 18:02:08 +0300

    Update treatment.py

--- treatment.py
+++ treatment.py
@@ -2,7 +2,7 @@ import pymongo
 import qsort
 
 client = pymongo.MongoClient()
-def trans (string):
+def trans (string, norm = True):
     j = 0
     k = 0
     l = 8
@@ -29,15 +29,48 @@ def trans (string):
                 ms = int(string[l:])
             k += 1
         j += 1
-    return ((((day*24 + hour)*60 + minute)*60 + second)*1000 + ms)
+    if norm:
+        return ((((day*24 + hour)*60 + minute)*60 + second)*1000 + ms)
+    else:
+        if hour < 10:
+            hour = '0' + str(hour)
+        else:
+            hour = str(hour)
+        if day < 10:
+            day = '0' + str(day)
+        else:
+            day = str(day)
+        if minute < 10:
+            minute = '0' + str(minute)
+        else:
+            minute = str(minute)
+        if second < 10:
+            second = '0' + str(second)
+        else:
+            second = str(second)
+        return hour + ':' + minute + ':' + second + ', '+ day + '.02.2018'
 
 user_sum_1 = trans(input("sat"))
 user_sum_2 = trans(input("tas"))
+
 viborka = []
 i = 0
 for men in client.tests.insertTest.find():
+    pre_sum = trans(men['Date/Time'], False)
     post_sum = trans(men['Date/Time'])
     if user_sum_1 <= post_sum <= user_sum_2:
-        viborka.append([post_sum,{'Method name' : men['Method name']}, {'App name' : men['App name']}])
+        viborka.append([pre_sum, post_sum,{'Method name' : men['Method name']}, {'App name' : men['App name']}])
+    i += 1
+    print(i)
+    if i > 100000:
+        break
 
-print(qsort.sort(viborka))
+f = open('text.txt', 'w')
+viborka = qsort.sort(viborka)
+
+j = 0
+for i in viborka:
+    f.write(str(j + 1) + ')' + '\n' + i[0] + '\n' 'Method name : ' + i[2]['Method name'] + '\n' + 'App name: ' + i[3]['App name'])
+    f.write('\n')
+    j += 1
+f.close()
